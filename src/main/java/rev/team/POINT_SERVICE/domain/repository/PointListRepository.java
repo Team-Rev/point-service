b5342ab986833c585e2faf517aa0c671b8ec6529
EunsilSon <eunsil1023@gmail.com>
2021-05-14 18:43:56 +0900

    feature/파일 명 변경

--- src/main/java/rev/team/POINT_SERVICE/domain/repository/PointListRepository.java
+++
@@ -1,9 +0,0 @@
-package rev.team.POINT_SERVICE.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rev.team.POINT_SERVICE.domain.entity.PointList;
-
-@Repository
-public interface PointListRepository extends JpaRepository<PointList, Integer> {
-}
